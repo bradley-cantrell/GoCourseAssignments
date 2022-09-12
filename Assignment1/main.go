3fcd8696eb11eeef1b0de7e3b1aaabbbe7570d24
bradley-cantrell <cantrellbldev@gmail.com>
2022-09-12 12:25:18 -0400

    Adds assignment 1

---
+++ Assignment1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, int := range ints {
+		if int%2 != 0 {
+			fmt.Printf("%v is odd\n", int)
+		}
+
+		if int%2 == 0 {
+			fmt.Printf("%v is even\n", int)
+		}
+	}
+}
